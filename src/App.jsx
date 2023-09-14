4cb17921726e0133bf4ac309b1a4c309ad933618
Toma Ilcic <toma.ilcic@gmail.com>
2023-09-14 17:24:43 +0200

    rjesenje

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
 import "./App.css";
 import { Component } from "react";
-import ClassComp from "./ClassComp";
-import FuncComp1 from "./FuncComp1";
-import FuncComp2 from "./FuncComp2";
-
-const users = [
-  { name: "Ivan", years: 30 },
-  { name: "Marko", years: 35 },
-  { name: "Ana", years: 25 },
-];
+// import ClassComp from "./ClassComp";
+// import FuncComp1 from "./FuncComp1";
+// import FuncComp2 from "./FuncComp2";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        { name: "Ivan", years: 30 },
+        { name: "Marko", years: 35 },
+        { name: "Ana", years: 25 },
+      ],
+      nekiText: "Ovo je neki text",
+    };
+  }
+
+  uvecaj = (index) => {
+    this.setState((prevState) => {
+      const newUsers = [...prevState.users];
+      newUsers[index].years += 1;
+      return { users: newUsers };
+    });
+  };
+
   render() {
     return (
-      <>
-        <FuncComp1 ime={users[0].name} godine={users[0].years}></FuncComp1>
-        <ClassComp ime={users[1].name} godine={users[1].years}></ClassComp>
-        <FuncComp2 />
-      </>
+      <div>
+        {this.state.users.map((user, index) => (
+          <div key={index}>
+            <p>
+              {user.name} has {user.years} years.
+            </p>
+            <button onClick={() => this.uvecaj(index)}>
+              Increase years for {user.name}
+            </button>
+          </div>
+        ))}
+        <p>{this.state.nekiText}</p>
+      </div>
     );
   }
 }
+
 export default App;
--- src/FuncComp2.jsx
+++ src/FuncComp2.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
-function FuncComp2({ ime, godine }) {
+function FuncComp2({ ime, godine, children }) {
   return (
     <>
       <p>
         {ime} ce za 10 godina imati {godine + 10}{" "}
       </p>
+      {children}
     </>
   );
 }
@@ -12,10 +13,11 @@ function FuncComp2({ ime, godine }) {
 FuncComp2.propTypes = {
   ime: PropTypes.string,
   godine: PropTypes.number,
+  children: PropTypes.element,
 };
 
-FuncComp2.defaultProps = {
-  godine: 25,
-  ime: "Ana",
-};
+// FuncComp2.defaultProps = {
+//   godine: 25,
+//   ime: "Ana",
+// };
 export default FuncComp2;
