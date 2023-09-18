d9d8ab082e5ccc4ae94eb0ab4b4d09c0c31ad03b
Toma Ilcic <toma.ilcic@gmail.com>
2023-09-18 17:53:44 +0200

    event rjesenje

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,11 @@ function App() {
     newUsers[index].years += 1;
     setUsers(newUsers);
   };
+  const changeName = (event, index) => {
+    const newUsers = [...users];
+    newUsers[index].name = event.target.value;
+    setUsers(newUsers);
+  };
 
   return (
     <div>
@@ -27,6 +32,7 @@ function App() {
           <p>
             {user.name} has {user.years} years.
           </p>
+          <input type="text" onChange={(event) => changeName(event, index)} />
           <button onClick={() => uvecaj(index)}>
             {nekiText}
             {user.name}
